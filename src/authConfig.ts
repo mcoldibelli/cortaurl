c8a899e74f6db37cbbd87a0c583ff2a9b69974b9
Marcelo Coldibelli <marcelo.coldibelli@gmail.com>
2025-05-27 13:44:20 -0300

    feat: implements auth provider

---
+++ src/authConfig.ts
@@ -0,0 +1,13 @@
+const cognitoAuthConfig = {
+  authority: import.meta.env.VITE_COGNITO_AUTHORITY,
+  client_id: import.meta.env.VITE_COGNITO_CLIENT_ID,
+  redirect_uri: import.meta.env.VITE_COGNITO_REDIRECT_URI,
+  response_type: "code",
+  scope: "email openid",
+};
+
+Object.entries(cognitoAuthConfig).forEach(([key, value]) => {
+  if (!value) throw new Error(`Missing env: ${key}`);
+});
+
+export default cognitoAuthConfig;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AuthProvider } from "react-oidc-context"
+import cognitoAuthConfig from './authConfig.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
